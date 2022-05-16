f1ba4c6977c7b7b4db43309c34540b51ef53c049
Bhargav Patel <patelbhargav808@gmail.com>
2022-05-16 19:54:15 -0400

    first commit

---
+++ src/main/java/calculatorApp/app.java
@@ -0,0 +1,21 @@
+package calculatorApp;
+
+import java.util.Scanner;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("**********---- Bhargav Calculator ----- ***************");
+        System.out.println();
+        calculator cal = new calculator();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        cal.add(a, b);
+        cal.mul(a, b);
+	}
+
+}
---
+++ src/main/java/calculatorApp/calculator.java
@@ -0,0 +1,15 @@
+package calculatorApp;
+
+public class calculator {
+
+	public int add(int i, int j) {
+        System.out.println("Addition of " + i + "," + j + " is: " + (i + j));
+        return i + j;
+    }
+public int mul(int i, int j) {
+        System.out.println("Multiplication of " + i + "," + j + " is: " + (i * j));
+        return i * j;
+    }
+
+}
+
