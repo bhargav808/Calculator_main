18a95e37c43414da503d259fe013488bf1f0d5ca
Bhargav Patel <patelbhargav808@gmail.com>
2022-05-18 15:54:20 -0400

    switch case

--- src/main/java/calculatorApp/app.java
+++ src/main/java/calculatorApp/app.java
@@ -11,15 +11,37 @@ public class app {
         calculator cal = new calculator();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two numbers: ");
-        int a, b;
+        int a, b , c;
         a = sc.nextInt();
         b = sc.nextInt();
-        cal.add(a, b);
-        cal.mul(a, b);
-        cal.sub(a, b);
-        cal.div(a, b);
-        sc.close();
+        System.out.println("1=Add, 2=Subtraction, 3=Multiply, 4=Divide");
+        c = sc.nextInt();
+        switch(c){
 
-	}
+        case 1:
+            System.out.println("you choose addition!");
+            cal.add(a, b);
+            break;
+
+        case 2:
+            cal.sub(a, b);
+            break;
+
+        case 3:
+            cal.mul(a, b);
+            break;
+
+        case 4:
+        	cal.div(a, b);
+            break;
+
+        default:
+            System.out.println("You did not make a valid choice, please run the program again.");
+            System.exit(0);
+        }
+  }
 
 }
+        
+
+ 
